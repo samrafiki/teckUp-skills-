bec91abb6cd303e0c4dd5a31f145c522a6d02acb
samrafiki <samrafiki02@gmail.com>
2022-11-30 10:41:37 +0200

    'challenges'

---
+++ second question.js
@@ -0,0 +1,16 @@
+var hm={'homes':["name":"Bike","price":"100"},{"name":"Tv","price":"200"},{"name":"Album","price":"100"},{"name":"Book","price":"5"},{"name":"Phone","price":"500"},{"name":"Computer","price":"1000"},]}
+var hmm=hm.filter(function hm(price) {
+	return hmm.price<="1000";});
+console.log(hmm);
+
+
+var hmm=hm.filter(function hm(price) {
+	return hmm.price>="200";});
+console.log(hmm);
+
+
+var hmm=hm.filter(function hm(price) {
+	return hmm.price<="1000*100*200*500*5";});
+console.log(hmm);
+	// body...
+})
